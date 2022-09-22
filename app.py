a39893dd8a2ac0ca78b4a8fa0e82e0117decf873
Denzel Wendy <denzel.wendy@binus.ac.id>
2022-09-22 14:27:38 +0700

    Add Database models

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 
 
 @app.route("/")
@@ -15,8 +15,21 @@ def hello():
 
 @app.route("/todo")
 def todo():
+    todo_list = Todo.query.all()
+    print(todo_list)
     return render_template("todo.html")
 
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40))
+    description = db.Column(db.String(100))
+    complete = db.Column(db.Boolean, default=False)
+
+
 if __name__ == "__main__":
+    # db.create_all()
+    # new_todo = Todo(title="Todo 2", description="hello world", complete=True)
+    # db.session.add(new_todo)
+    # db.session.commit()
     app.run(debug=True)
